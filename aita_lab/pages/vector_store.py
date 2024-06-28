f95af6a6ac0aa06a7e833874be4f4acd6f574061
cmuhao <sduxuhao@gmail.com>
2024-06-28 00:32:40 -0700

    update vector db
    Signed-off-by: cmuhao <sduxuhao@gmail.com>

--- aita_lab/pages/vector_store.py
+++ aita_lab/pages/vector_store.py
@@ -2,6 +2,7 @@
 
 import reflex as rx
 
+from aita_lab import styles
 from aita_lab.states.vector_store import VectorStoreState
 from aita_lab.templates import ThemeState, template
 
@@ -9,10 +10,10 @@ from aita_lab.templates import ThemeState, template
 def add_feature():
     return rx.dialog.root(
         rx.dialog.trigger(
-            rx.button(
-                rx.flex("Add Feature", rx.icon(tag="plus", width=24, height=24), spacing="3"),
-                size="4",
-                radius="full",
+            rx.chakra.button(
+                rx.flex("Add Feature +", spacing="3"),
+                size="sm",
+                width="100%",
                 on_click=VectorStoreState.open_feature_dialog,
             ),
         ),
@@ -24,24 +25,48 @@ def add_feature():
                 padding_top="1em",
             ),
             rx.dialog.description(
-                "Select the feature and enter the feature information for your data source",
+                "Create a feature to store your embedding data.",
                 size="2",
                 mb="4",
                 padding_bottom="1em",
             ),
             rx.flex(
-                rx.text(
-                    "Name *",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
+                rx.chakra.heading("Feature Name *", size="sm"),
                 rx.input(
                     placeholder="Enter the name of the feature",
-                    on_blur=VectorStoreState.set_name,
+                    on_blur=VectorStoreState.set_feature_name,
                     required=True,
                 ),
+                rx.chakra.heading("Schema *", size="sm"),
+                rx.input(
+                    placeholder="Enter the schema",
+                    on_blur=VectorStoreState.set_feature_schema,
+                    required=True,
+                ),
+                rx.chakra.heading("Entities", size="sm"),
+                rx.input(
+                    placeholder="Enter the entities",
+                    on_blur=VectorStoreState.set_feature_entities,
+                    required=False,
+                ),
+                rx.chakra.heading("Source", size="sm"),
+                rx.input(
+                    placeholder="Enter the source",
+                    on_blur=VectorStoreState.set_feature_source,
+                    required=False,
+                ),
+                rx.chakra.heading("ttl", size="sm"),
+                rx.input(
+                    placeholder="Enter the ttl",
+                    on_blur=VectorStoreState.set_feature_ttl,
+                    required=False,
+                ),
+                rx.chakra.button(
+                    "Create Feature",
+                    on_click=VectorStoreState.create_vector_feature,
+                ),
+                direction="column",
+                spacing="4",
             ),
         ),
     )
@@ -71,30 +96,19 @@ def setup_store():
                 padding_bottom="1em",
             ),
             rx.flex(
-                rx.text(
-                    "Name *",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
+                rx.chakra.heading("Project Name *", size="sm"),
                 rx.input(
                     placeholder="Enter the name of the project",
                     on_blur=VectorStoreState.set_project_name,
                     required=True,
                 ),
-                rx.text(
-                    "Online Store *",
-                    as_="div",
-                    size="2",
-                    mb="1",
-                    weight="bold",
-                ),
+                rx.chakra.heading("Online Store Type *", size="sm"),
                 rx.select(
                     ["Postgresql", "Sqlite", "Elasticsearch"],
                     placeholder="Select the store type",
                     on_change=VectorStoreState.set_online_store_type,
                 ),
+                rx.chakra.heading("Online Store Configs", size="sm"),
                 rx.input(
                     placeholder="Enter the vector online store configs",
                     on_blur=VectorStoreState.set_online_store_configs,
@@ -110,7 +124,88 @@ def setup_store():
     )
 
 
-@template(route="/vector_store", title="Vector Store")
+def show_features():
+    return rx.chakra.box(
+        rx.chakra.heading("Embedding Features"),
+        rx.chakra.text("Create a feature to store your embedding data"),
+        rx.chakra.table_container(
+            rx.chakra.table(
+                rx.chakra.thead(
+                    rx.chakra.tr(
+                        rx.chakra.th("Name"),
+                        rx.chakra.th("Entities"),
+                        rx.chakra.th("schema"),
+                        rx.chakra.th("Source"),
+                        rx.chakra.th("ttl"),
+                        rx.chakra.th("Load"),
+                    )
+                ),
+                rx.chakra.tbody(
+                    rx.foreach(
+                        VectorStoreState.vector_features,
+                        lambda feature: rx.chakra.tr(
+                            rx.chakra.td(feature.name),
+                            rx.chakra.td(feature.entities),
+                            rx.chakra.td(feature.schema),
+                            rx.chakra.td(feature.source),
+                            rx.chakra.td(feature.ttl),
+                            rx.chakra.td(rx.chakra.button(
+                                "Load",
+                                on_click=VectorStoreState.load_vector_feature,
+                            ))
+                        ),
+                    ),
+                    width="100%",
+                ),
+                color_scheme="white",
+                variant="striped",
+                width="100%",
+            ),
+            add_feature(),
+            width="100%",
+        ),
+        width="100%",
+        padding="1em",
+    )
+
+
+def show_store():
+    return rx.chakra.box(
+        rx.tabs.root(
+            rx.tabs.list(
+                rx.tabs.trigger(
+                    VectorStoreState.project_name,
+                    value="tab1",
+                    cursor="pointer",
+                ),
+                rx.tabs.trigger(
+                    "Tab 2",
+                    value="tab2",
+                    cursor="pointer",
+                ),
+            ),
+            rx.tabs.content(
+                show_features(),
+                value="tab1",
+                width="100%",
+            ),
+            rx.tabs.content(
+                rx.text("item on tab 2"),
+                value="tab2",
+                width="100%",
+            ),
+            default_value="tab1",
+            orientation="horizontal",
+            height="100vh",
+            width="100%",
+        ),
+        width="100%",
+        border=styles.border,
+        border_radius=styles.border_radius,
+    )
+
+
+@template(route="/vector_store", title="Vector Store", on_load=[VectorStoreState.on_load()])
 def vector_store() -> rx.Component:
     """The Vector Store page.
 
@@ -121,4 +216,6 @@ def vector_store() -> rx.Component:
         rx.chakra.heading("Vector Store"),
         rx.chakra.text("Create a vector store to store your embedding features"),
         setup_store(),
+        show_store(),
+        width="100%",
     )
--- aita_lab/states/vector_store.py
+++ aita_lab/states/vector_store.py
@@ -1,10 +1,20 @@
+import json
+import logging
+import os
+from pathlib import Path
 from typing import Optional
 
+import pandas as pd
 import reflex as rx
+from feast import FeatureView, FileSource
+from feast.data_format import ParquetFormat
+from feast.data_source import PushSource
 from feast.feature_store import FeatureStore
 from feast.repo_config import RepoConfig
-
+from feast.repo_operations import _prepare_registry_and_repo, apply_total_with_repo_instance
 from aita_lab.states.base import BaseState
+from feast.field import Field
+from feast.types import Array, Float32
 
 
 class VectorStore(rx.Model):
@@ -18,14 +28,30 @@ class VectorStore(rx.Model):
     project_configs: Optional[dict]
 
 
+class FeastFeature(rx.Model):
+    name: str
+    schema: str
+    entities: Optional[str] = ""
+    source: Optional[str] = ""
+    ttl: Optional[int] = None
+
+
 class VectorStoreState(BaseState):
-    project_name: str
+    project_name: str = "test"
     project_configs: Optional[dict]
 
-    online_store_type: str
-    offline_store_type: Optional[str] = ""
-    online_store_configs: Optional[dict]
-    offline_store_configs: Optional[dict]
+    online_store_type: str = "sqlite"
+    offline_store_type: Optional[str] = "file"
+    online_store_configs: dict = {"path": "data/vector.db"}
+    offline_store_configs: Optional[dict] = {}
+
+    feature_name: str = "test"
+    feature_schema: str = "test_schema"
+    feature_entities: Optional[str] = None
+    feature_source: Optional[str] = ""
+    feature_ttl: Optional[int] = None
+
+    vector_features: list[FeastFeature] = []
 
     store_dialog_open: bool = False
     feature_dialog_open: bool = False
@@ -38,25 +64,107 @@ class VectorStoreState(BaseState):
     def open_store_dialog(self):
         self.store_dialog_open = not self.store_dialog_open
 
+    def set_online_store_configs(self, configs):
+        self.online_store_configs = json.loads(configs)
+
+    def _get_feast_repo_config(self):
+        return RepoConfig(
+            project=self.project_name,
+            registry="data/vector.db",
+            provider="local",
+            online_config={
+                **self.online_store_configs,
+                "type": self.online_store_type,
+            },
+            offline_config={
+                **self.offline_store_configs,
+                "type": self.offline_store_type,
+            },
+            entity_key_serialization_version=3
+        )
+
     def create_store(self):
-        self._fs = FeatureStore(
-            config=RepoConfig(
-                project=self.project_name,
-                registry="/reflex.db",
-                provider="local",
-                online_store={
-                    "type": self.online_store_type,
-                    **self.online_store_configs,
-                },
-                offline_store={
-                    "type": self.offline_store_type,
-                    **self.offline_store_configs,
-                },
+        repo_config = self._get_feast_repo_config()
+        repo_path = Path(os.path.join(Path.cwd(), "data"))
+        try:
+            project, registry, repo, store = _prepare_registry_and_repo(repo_config, repo_path)
+            apply_total_with_repo_instance(
+                store, project, registry, repo, True
+            )
+            self._fs = FeatureStore(config=repo_config)
+            logging.info("Feature store created")
+        except Exception as e:
+            logging.error(f"Error creating feature store: {e}")
+            raise e
+        finally:
+            self.open_store_dialog()
+
+    def create_feature_schema(self):
+        return [Field(name=self.feature_schema, dtype=Array(Float32))]
+
+    def create_feature_source(self):
+        return PushSource(
+            name=self.feature_source,
+            batch_source=FileSource(
+                file_format=ParquetFormat(),
+                path="data/feature.parquet",
+                timestamp_field="event_timestamp",
             )
         )
 
-    def create_feature(self, feature):
-        pass
+    def create_vector_feature(self):
+        self._fs.apply(FeatureView(
+            name=self.feature_name,
+            entities=self.feature_entities,
+            schema=self.create_feature_schema(),
+            source=self.create_feature_source(),
+            ttl=self.feature_ttl
+        ))
+        logging.info(f"Feature {self.feature_name} created")
+        self.open_feature_dialog()
 
     def _embed(self, text: str):
         pass
+
+    def load_store(self):
+        repo_config = self._get_feast_repo_config()
+        self._fs = FeatureStore(config=repo_config)
+        self.project_name = self._fs.project
+        self.vector_features = []
+        for feature_view in self._fs.list_feature_views():
+            feature_spec = self._fs.get_feature_view(feature_view.name)
+            self.vector_features.append(FeastFeature(
+                name=feature_spec.name,
+                entities=", ".join(feature_spec.entities),
+                schema=", ".join([f"{schema.name}:{schema.dtype}" for schema in feature_spec.schema]),
+                source=feature_spec.batch_source.name,
+                ttl=feature_spec.ttl.seconds if feature_spec.ttl else None
+            ))
+        logging.info("Feature store loaded")
+
+    def load_vector_feature(self):
+        feature_data_frame = pd.DataFrame(
+            {
+                "test_schema": [[1.0, 2.0, 3.0]],
+                "event_timestamp": pd.to_datetime(["2021-01-01"]),
+                "__dummy_id": ["1"],
+            }
+        )
+        try:
+            self._fs.push(self.feature_source, feature_data_frame)
+            logging.info(f"Feature {self.feature_name} loaded")
+        except Exception as e:
+            logging.error(f"Error loading feature: {e}")
+            raise e
+
+    def retrieve_vector_features(self, feature: str, query, top_k: int = 3) -> dict:
+        logging.info(f"Retrieving online documents for {feature} with query {query}")
+        response = self._fs.retrieve_online_documents(
+            feature=feature,
+            query=query,
+            top_k=top_k
+        )
+        return response.to_dict()
+
+    def on_load(self) -> None:
+        self.load_store()
