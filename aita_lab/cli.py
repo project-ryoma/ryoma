b5a20b4dea7e315f7efea949642e232c70593073
cmuhao <sduxuhao@gmail.com>
2024-07-23 15:26:09 -0700

    fix linting
    Signed-off-by: cmuhao <sduxuhao@gmail.com>

--- aita_lab/cli.py
+++ aita_lab/cli.py
@@ -1,24 +1,6 @@
-from typer import Typer
-from subprocess import Popen, PIPE
-
-cli = Typer()
-
-
-@cli.command()
-def start():
-    process = Popen(["reflex", "run"], shell=False)
-    process.communicate()
-
-
-@cli.command()
-def migrate():
-    process = Popen(["reflex", "migrate"], shell=False)
-    process.communicate()
+from reflex.reflex import cli
 
 
 def main():
     cli()
 
-
-if __name__ == "__main__":
-    main()
